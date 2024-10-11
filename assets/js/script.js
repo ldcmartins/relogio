8f2da5310cd0c17a31204296d66d28d0707c2d1a
ldcmartins <ldcmartins3@gmail.com>
2024-10-10 23:47:57 -0300

    finishjs-project

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,18 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    // Adicionar zero à esquerda se for menor que 10
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+}, 1000); // Atualiza o relógio a cada 1 segundo (1000 milissegundos)
